a08d0946afafa39638019cd755c5189e8d3d71ab
Alonso Manilla <alonso.manilla@gmail.com>
2022-01-31 11:01:57 -0600

    Mutations with id return

--- resolvers.js
+++ resolvers.js
@@ -1,13 +1,45 @@
 const db = require('./db')
 const Mutation = {
-   createStudent:(root,args,context,info) => {
-      return db.students.create({collegeId:args.collegeId,
-      firstName:args.firstName,
-      lastName:args.lastName})
+   createStudent:(root, args, context, info) => {
+      return db.students.create({
+         collegeId: args.collegeId,
+         firstName: args.firstName,
+         lastName: args.lastName})
+   },
+
+   addStudent: (root, args, context, info) => {
+      const id = db.students.create({
+         collegeId: args.collegeId,
+         firstName: args.firstName,
+         lastName: args.lastName
+      })
+      return db.students.get(id);
+   },
+
+   addCollege: (root, args, context, info) => {
+      const id = db.colleges.create({
+         name: args.name,
+         location: args.location,
+         rating: args.rating
+      })
+      return db.colleges.get(id);
    }
 }
-const Query = {
-   greeting:() => "hello"
+
+//for each single student object returned,resolver is invoked
+const Student = {
+   college:(root) => {
+      return db.colleges.get(root.collegeId);
+   }
 }
 
-module.exports = {Query,Mutation}
+const Query = {
+   studentById: (root, args, context, info) => {
+      return db.students.get(args.id);
+   },
+   colleges: () => {
+      return db.colleges.list();
+   }
+}
+
+module.exports = {Query, Student, Mutation}
